b1932b65625cad8eafa724edf6980934f26b3e43
mahajant99 <mahajant99@gmail.com>
2020-04-30 12:39:33 +0530

    sakura: Update Blobs From daisy-user-10-QKQ1.191002.002-V11.0.4.0.QDLMIXM-release-keys * Updated Camera Blobs * Updated Fingerprint Blobs
    Signed-off-by: mahajant99 <mahajant99@gmail.com>

--- device.mk
+++ device.mk
@@ -42,6 +42,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.postproc@1.0 \
     Snap
 
 # ConsumerIr
--- prop.mk
+++ prop.mk
@@ -7,10 +7,20 @@ ro.vendor.audio.sdk.fluencetype=fluence
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
+vidc.enc.dcvs.extra-buff-count=2 \
+media.camera.ts.monotonic=1 \
+persist.vendor.camera.display.lmax=1280x720 \
+persist.vendor.camera.display.umax=1920x108 \
+vendor.camera.hal1.packagelist=com.skype.raider,com.google.android.talk,com.whatsapp \
+vendor.camera.lowpower.record.enable=1 \
+vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.huaqin.factory,com.mi.AutoTest \
+vendor.camera.aux.packagelist2=com.android.systemui,com.huaqin.cameraautotest,com.huaqin.runtime \
 vendor.camera.aux.packageblacklist=com.discord \
-persist.vendor.camera.HAL3.enabled=1 \
-vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera2,com.google.android.GoogleCamera \
-vendor.camera.aux.packagelist2=com.google.android.GoogleCameraWide,com.dual.GCam,com.Wide.GCam,com.Tele.GCam \
-vendor.camera.hal1.packagelist=com.whatsapp \
-persist.camera.dual.camera=0 \
-persist.camera.eis.enable=1
+persist.vendor.qti.telephony.vt_cam_interface=2 \
+persist.vendor.camera.dual.camera=0 \
+persist.vendor.camera.eis.enable=1 \
+persist.vendor.camera.gyro.disable=0 \
+persist.vendor.camera.isp.clock.optmz=0 \
+persist.vendor.camera.stats.test=5 \
+persist.vendor.camera.CDS=off \
+persist.camera.HAL3.enabled=1
