ed60838a3d8857903350cd5472d2241fe69d8ab1
mahajant99 <mahajant99@gmail.com>
2020-05-01 17:16:24 +0530

    sakura: Havoc-ify
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- prop.mk
+++ prop.mk
@@ -24,3 +24,7 @@ persist.vendor.camera.isp.clock.optmz=0 \
 persist.vendor.camera.stats.test=5 \
 persist.vendor.camera.CDS=off \
 persist.camera.HAL3.enabled=1
+
+# Havoc
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.havoc.maintainer=Tushar Mahajan(mahajant99)
