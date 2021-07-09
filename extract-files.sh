0513858631ce8a5215905e2ed9470e1f7fbd72dc
ItsVixano <vixanonew@gmail.com>
2021-07-09 15:31:59 +0200

    sakura: Roll back Fingerprint Firmwares to daisy V10.0.20.0.PDLMIXM
    * Sadly those comes from "modem/image", no clue why xiaomi done this
    * Also edit libgf_ca.so to look in /vendor/firmware instead of /system/etc/firmware
    
    Co-Authored-by: Akhil Narang <akhilnarang.1999@gmail.com>
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>
    Change-Id: Ia41d5ab911067e18ca8d1cb874e348c7d6eb709e

--- extract-files.sh
+++ extract-files.sh
@@ -15,7 +15,25 @@
 # limitations under the License.
 #
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib64/libgf_ca.so)
+            sed -i 's|/system/etc/firmware|/vendor/firmware\x0\x0\x0\x0|g' "${2}"
+            ;;
+        vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so)
+            "${PATCHELF_0_8}" --remove-needed "libprotobuf-cpp-lite.so" "${2}"
+            ;;
+    esac
+}
+
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
+
 set -e
+
 
 # Required!
 export DEVICE=sakura
