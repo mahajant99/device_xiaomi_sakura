484451bb42a33c651a7770820326c845b300bd0d
ItsVixano <vixanonew@gmail.com>
2021-07-11 16:44:30 +0200

    sakura: Switch to OSS camera hal
    Change-Id: Iad5961ebe453ce822458df5f68b2d6992c7730f6
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- device.mk
+++ device.mk
@@ -35,13 +35,12 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.5 \
-    android.hardware.camera.device@3.4 \
-    vendor.qti.hardware.camera.device@1.0.vendor \
+    camera.msm8953 \
     libdng_sdk.vendor \
+    libmm-qcamera \
+    libstdc++.vendor \
     Snap
 
 # ConsumerIr
